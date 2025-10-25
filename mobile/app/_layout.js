0cbb6a4ec38bcd00feb6df836dbd54f57cde5daf
Ray Mondragon <raymondragonn@gmail.com>
2025-10-25 09:14:05 -0600

    eliminado de scan-qr y sus dependencias

--- mobile/app/_layout.js
+++ mobile/app/_layout.js
@@ -23,14 +23,93 @@ export default function TabLayout() {
           fontWeight: 'bold',
         },
       }}
+      initialRouteName="home"
     >
       <Tabs.Screen
         name="home"
         options={{
-          title: '',
+          title: 'Inicio',
           tabBarIcon: ({ color, size }) => (
             <Ionicons name="home" size={size} color={color} />
           ),
+        }}
+      />
+      <Tabs.Screen
+        name="index"
+        options={{
+          href: null,
+        }}
+      />
+      <Tabs.Screen
+        name="bills"
+        options={{
+          href: null,
+        }}
+      />
+      <Tabs.Screen
+        name="groups"
+        options={{
+          href: null,
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          href: null,
+        }}
+      />
+      <Tabs.Screen
+        name="pago-completado"
+        options={{
+          href: null,
+        }}
+      />
+      <Tabs.Screen
+        name="auth/login"
+        options={{
+          href: null,
+        }}
+      />
+      <Tabs.Screen
+        name="auth/register"
+        options={{
+          href: '/auth/register',
+        }}
+      />
+      <Tabs.Screen
+        name="user/productos"
+        options={{
+          href: null,
+        }}
+      />
+      <Tabs.Screen
+        name="user/total"
+        options={{
+          href: null,
+        }}
+      />
+      <Tabs.Screen
+        name="admin/qr"
+        options={{
+          href: null,
+        }}
+      />
+      <Tabs.Screen
+        name="admin/pago"
+        options={{
+          href: null,
+        }}
+      />
+      <Tabs.Screen
+        name="admin/integrantes"
+        options={{
+          href: null,
+        }}
+      />
+      <Tabs.Screen
+        name="admin/escanear-ticket"
+        options={{
+          href: null,
         }}
       />
     </Tabs>
